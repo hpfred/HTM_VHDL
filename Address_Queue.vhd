c64152fea386a670a021c7d5be5cb7b6e1ce7798
hpfred <frepantunes@yahoo.com.br>
2023-06-15 00:28:23 -0300

    Na verdade acho que tava adicionando componente aonde não precisa, então vou commitar isso e remover

--- Address_Queue.vhd
+++ Address_Queue.vhd
@@ -20,16 +20,24 @@ TYPE TRANS_ADDR_MEM IS ARRAY (X DOWNTO 0) OF STD_LOGIC;		--Limite de X endereço
 TYPE ALL_DATA IS ARRAY (3 DOWNTO 0) OF TRANS_ADDR_MEM;
 SIGNAL MemStorage: ALL_DATA;
 
-COMPONENT Memes IS
+COMPONENT Control_Unit IS
 	PORT
 	(
-		tipos: in
+		Action:			IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+		MemAddress:		IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		Data:				IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		ProcID:			IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+		TransactionID:	IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+		
+		TransactionStatus:	OUT STD_LOGIC_VECTOR (X DOWNTO 0);
+		
+		Clock:	IN STD_LOGIC
 	);
 END COMPONENT;
 BEGIN
-	X: Y PORT MAP (
-						a=>b
-						);
+	Control: Control_Unit PORT MAP (
+											  Action=>
+											  );
 
 	--Se Push coloca ADDR no próximo espaço disponível do array TrID da Memória
 	--Se Pull coloca o primeiro valor do array TrID da Memória no Ret, e avança uma posição todos valores desse array (sobresecrevendo esse primeiro salvo no Ret - porém cuidar pra quando fazer isso n acabar fazendo Ret receber um fio conectando o valor do Array, de forma que quando remove ele o Ret fica errado)
